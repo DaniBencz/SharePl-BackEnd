6f469c7953c4d408d66577351c0ac9ff5136883b
Daniel <daniel.bencz@gmail.com>
2019-12-11 09:21:05 +0100

    sql connection

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+require('dotenv').config();
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  multipleStatements: true
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
-  //console.log(`Database is ${conn === null ? 'NOT ' : ''}available`);
+  console.log(`Database is ${conn === null ? 'NOT ' : ''}available`);
 });
